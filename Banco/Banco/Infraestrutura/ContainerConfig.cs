2e7c270fe60a537edda9f1c9cbb71f3c690b31da
raulsantiago <raulsantiago@id.uff.br>
2019-10-02 17:57:13 -0300

    Container Cauam

---
+++ Banco/Banco/Infraestrutura/ContainerConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Banco.Aplicacao.CasoDeUso;
+using Autofac;
+using Mono.CSharp;
+
+namespace Banco.Infraestrutura
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Deposito>().As<IDeposito>();
+            builder.RegisterType<Retirada>().As<IRetirada>();
+            builder.RegisterType<Transferencia>().As<ITransferencia>();
+
+            return builder.Build();
+        }
+    }
+}
--- Banco/Banco/Program.cs
+++ Banco/Banco/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Banco.Aplicacao.CasoDeUso;
 using Banco.Dominio;
+using Banco.Infraestrutura;
 using System;
 
 namespace Banco
@@ -10,90 +11,66 @@ namespace Banco
         public static void Main(string[] args)
         {
             // Aqui ele busca a classe concreta exemplo Deposito
-            var builder = new ContainerBuilder();            
-            builder.RegisterType<Deposito>()
-                .As<IDeposito>() // a classe IDeposito é a classe Deposito
-                .InstancePerDependency();
-            IContainer container = builder.Build();
             // Aqui roda o teste de Deposito em produção
-            Cliente x;
-            Conta y;          
-            x = new PessoaFisica("Raul Santiago", "530.280.270-87");
-            y = new Conta(0838, 12345678, EnumTipoConta.Poupanca, x);            
-            IDeposito dr = new Deposito();
-            dr.Executar(10.5m, y);
+            IContainer container = ContainerConfig.Configure();
+            Cliente Primeiro;
+            Conta PrimeiraConta;
+            Primeiro = new PessoaFisica("Raul Santiago", "530.280.270-87");
+            PrimeiraConta = new Conta(0838, 12345678, EnumTipoConta.Poupanca, Primeiro);
+            container.Resolve<IDeposito>().Executar(10.5m, PrimeiraConta);
             Console.WriteLine("++++++++DEPOSITO+++++++++");
-            Console.WriteLine("Saldo atual após depósito: R$ " + y.Saldo.ToString("F2") + " do " + x.ToString());
+            Console.WriteLine("Saldo atual após depósito: R$ " + PrimeiraConta.Saldo.ToString("F2") + " do " + Primeiro.ToString());
             Console.WriteLine("++++EXTRATO++++");
-            Console.WriteLine("Nº Agência: "+y.Agencia+" Nº Conta: "+y.NumConta);
-            foreach (Extrato extrato in y.Extratos)
+            Console.WriteLine("Nº Agência: "+ PrimeiraConta.Agencia+" Nº Conta: "+ PrimeiraConta.NumConta);
+            foreach (Extrato extrato in PrimeiraConta.Extratos)
             {   
                 Console.WriteLine(extrato);
             }
 
-            // Aqui ele busca a classe concreta exemplo Deposito
-            var builder2 = new ContainerBuilder();            
-            builder2.RegisterType<Retirada>()
-                .As<IRetirada>() // a classe IRetirada é a classe Retirada
-                .InstancePerDependency();
-            IContainer container2 = builder2.Build();
-            // Aqui roda o teste de Retirada em produção
-            Cliente a;
-            Conta b;
-            a = new PessoaJuridica("Empresa Luar", "14.234.432/0001-80");
-            b = new Conta(0026, 0406363, EnumTipoConta.Corrente, a);
-            IRetirada dr2 = new Retirada();
-            var c = dr2.Executar(1.95m, b);
+            // Aqui ele busca a classe concreta exemplo Deposito e roda o teste de Retirada em produção
+            Cliente Segundo;
+            Conta SegundaConta;
+            Segundo = new PessoaJuridica("Empresa Luar", "14.234.432/0001-80");
+            SegundaConta = new Conta(0026, 0406363, EnumTipoConta.Corrente, Segundo);
+            container.Resolve<IRetirada>().Executar(1.95m, SegundaConta);
             Console.WriteLine();
             Console.WriteLine("++++++++RETIRADA/SAQUE+++++++++");
-            Console.WriteLine("Saldo atual após retirada: R$ " + c.Saldo.ToString("F2")+" do "+a.ToString());            
+            Console.WriteLine("Saldo atual após retirada: R$ " + SegundaConta.Saldo.ToString("F2")+" do "+ Segundo.ToString());            
             Console.WriteLine("++++EXTRATO++++");
-            Console.WriteLine("Nº Agência: " + b.Agencia + " Nº Conta: " + b.NumConta);
-            foreach (Extrato extrato in b.Extratos)
+            Console.WriteLine("Nº Agência: " + SegundaConta.Agencia + " Nº Conta: " + SegundaConta.NumConta);
+            foreach (Extrato extrato in SegundaConta.Extratos)
             {
                 Console.WriteLine(extrato);
             }
             
-            var builder4 = new ContainerBuilder();
-            builder4.RegisterType<Retirada>()
-                .As<IRetirada>() 
-                .InstancePerDependency();
-            IContainer container4 = builder4.Build();           
-            IRetirada dr4 = new Retirada();
-            dr4.Executar(2.00m, y);
+            container.Resolve<IRetirada>().Executar(2.00m, PrimeiraConta);
             Console.WriteLine();
             Console.WriteLine("++++++++RETIRADA+++++++++");
-            Console.WriteLine("Saldo atual após retirada: R$ " + y.Saldo.ToString("F2") + " do " + x.ToString());
+            Console.WriteLine("Saldo atual após retirada: R$ " + PrimeiraConta.Saldo.ToString("F2") + " do " + Primeiro.ToString());
             Console.WriteLine("++++EXTRATO++++");
-            Console.WriteLine("Nº Agência: " + y.Agencia + " Nº Conta: " + y.NumConta);
-            foreach (Extrato extrato in y.Extratos)
+            Console.WriteLine("Nº Agência: " + PrimeiraConta.Agencia + " Nº Conta: " + PrimeiraConta.NumConta);
+            foreach (Extrato extrato in PrimeiraConta.Extratos)
             {
                 Console.WriteLine(extrato);
             }
 
-            var builder3 = new ContainerBuilder();
-            builder3.RegisterType<Transferencia>()
-                .As<ITransferencia>() // a classe IRetirada é a classe Retirada
-                .InstancePerDependency();
-            IContainer container3 = builder3.Build();            
-            ITransferencia tr = new Transferencia();
-            tr.Executar(5.0m, y, b);
+            container.Resolve<ITransferencia>().Executar(5.0m, PrimeiraConta, SegundaConta);
             Console.WriteLine();
             Console.WriteLine("++++++++TRANSFERENCIA+++++++++");
-            Console.WriteLine("Saldo R$" + y.Saldo.ToString("F2") + " DEPOIS da tranferencia do " + x.ToString());
+            Console.WriteLine("Saldo R$" + PrimeiraConta.Saldo.ToString("F2") + " DEPOIS da tranferencia do " + Primeiro.ToString());
             Console.WriteLine("++++EXTRATO++++");
-            Console.WriteLine("Nº Agência: " + y.Agencia + " Nº Conta: " + y.NumConta);
-            foreach (Extrato extrato in y.Extratos)
+            Console.WriteLine("Nº Agência: " + PrimeiraConta.Agencia + " Nº Conta: " + PrimeiraConta.NumConta);
+            foreach (Extrato extrato in PrimeiraConta.Extratos)
             {
                 Console.WriteLine(extrato);
             }
 
             Console.WriteLine();
             Console.WriteLine("++++++++SALDO+++++++++");
-            Console.WriteLine("Saldo atual após transferência: R$ " + c.Saldo.ToString("F2") + " do " + a.ToString());
+            Console.WriteLine("Saldo atual após transferência: R$ " + SegundaConta.Saldo.ToString("F2") + " do " + Segundo.ToString());
             Console.WriteLine("++++EXTRATO++++");
-            Console.WriteLine("Nº Agência: " + b.Agencia + " Nº Conta: " + b.NumConta);
-            foreach (Extrato extrato in b.Extratos)
+            Console.WriteLine("Nº Agência: " + SegundaConta.Agencia + " Nº Conta: " + SegundaConta.NumConta);
+            foreach (Extrato extrato in SegundaConta.Extratos)
             {
                 Console.WriteLine(extrato);
             }
